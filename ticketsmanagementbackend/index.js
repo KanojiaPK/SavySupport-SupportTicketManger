44677bf9b61b1cad04dd1b6af8d3e532006a448a
Prince Kanojia <princekanojia7000@gmail.com>
2024-11-13 21:09:05 +0530

    aaaaa

--- ticketsmanagementbackend/index.js
+++ ticketsmanagementbackend/index.js
@@ -8,11 +8,20 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8003;
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/supportTicketDB";
+const mongoURI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/supportTicketDB";
 
 // CORS headers (placed at the top)
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend URL in production
+  if (process.env.NODE_ENV === "production") {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://savysupport-supportticketmangerfrontend.onrender.com/"
+    );
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  }
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
